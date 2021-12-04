9f91937279b7c386c6926fdb9275cbb2dcf41f33
codeitsoku <32228326+codeitsoku@users.noreply.github.com>
2021-12-05 01:54:36 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
   //API_URL:"https://webapplication220211205000930.azurewebsites.net/api/",
   //PHOTO_URL:"https://webapplication220211205000930.azurewebsites.net/Photos/"
   runtimeConfigUrl: 'https://friendly-edison-83ea13.netlify.app/netlify/functions/get-runtime-config-data-function',
