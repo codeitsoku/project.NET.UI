a5ded7854d62c598c5e4713e1c3003e84fb14679
codeitsoku <32228326+codeitsoku@users.noreply.github.com>
2021-12-05 01:30:40 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  API_URL:"https://webapplication220211205000930.azurewebsites.net/api/SportsDepartment",
+  API_URL:"https://webapplication220211205000930.azurewebsites.net/api/",
   PHOTO_URL:"https://webapplication220211205000930.azurewebsites.net/Photos/"
 };
 
