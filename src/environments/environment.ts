8f27bc21b87be02a0316edc5e861dcab34915e27
codeitsoku <32228326+codeitsoku@users.noreply.github.com>
2021-12-05 01:16:23 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   API_URL:"https://webapplication220211205000930.azurewebsites.net/api/SportsDepartment",
   PHOTO_URL:"https://webapplication220211205000930.azurewebsites.net/Photos/"
 };
